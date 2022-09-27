4120358931f9a36885c909fddf58475ba26acc86
Anatoly <kovrigin.anatoliy@gmail.com>
2022-09-27 21:35:01 +0300

    added code to keyboard.js and deleted html-code keyboard from files html

---
+++ src/js/keyboard.js
@@ -0,0 +1,80 @@
+$(function ($) {
+
+    const body = $("body")
+    const modal = $('<div class="modal" id="keyboard" tabindex="-1" aria-labelledby="keyboard_label" aria-hidden="true">')
+    const modal_dialog = $('<div class="modal-dialog modal-dialog-centered" id="modal-dialog">')
+    const modal_content = $('<div class="modal-content">')
+    const modal_body = $('<div class="modal-body">')
+    const container_lg = $('<div class="container-lg">')
+
+    const row_1 = $('<div class="row d-flex justify-content-center">')
+    const col_1_1 = $('<div class="col d-flex justify-content-center m-2">')
+    const keyboard_input_id = $('<button id="keyboard_input" class="fs-1 w-100 btn" disabled><span></span></button>')
+
+    const row_2 = $('<div class="row d-flex justify-content-center">')
+    const col_2_1 = $('<div class="col d-flex justify-content-center m-1">')
+    const one_id = $('<button id="one" class="fs-1 w-100 btn m-1 clickable" type="button"><span>1</span></button>')
+    const col_2_2 = $('<div class="col d-flex justify-content-center m-1">')
+    const two_id = $('<button id="two" class="fs-1 w-100 btn m-1 clickable" type="button"><span>2</span></button>')
+    const col_2_3 = $('<div class="col d-flex justify-content-center m-1">')
+    const three_id = $('<button id="three" class="fs-1 w-100 btn m-1 clickable" type="button"><span>3</span></button>')
+    const col_2_4 = $('<div class="col d-flex justify-content-center m-1">')
+    const backspace_id = $('<button id="backspace" class="fs-1 w-100 btn m-1 clickable" type="button"><i class="fa fa-solid fa-delete-left"></i></button>')
+
+    const row_3 = $('<div class="row d-flex justify-content-center">')
+    const col_3_1 = $('<div class="col d-flex justify-content-center m-1">')
+    const four_id = $('<button id="four" class="fs-1 w-100 btn m-1 clickable" type="button"><span>4</span></button>')
+    const col_3_2 = $('<div class="col d-flex justify-content-center m-1">')
+    const five_id = $('<button id="five" class="fs-1 w-100 btn m-1 clickable" type="button"><span>5</span></button>')
+    const col_3_3 = $('<div class="col d-flex justify-content-center m-1">')
+    const six_id = $('<button id="six" class="fs-1 w-100 btn m-1 clickable" type="button"><span>6</span></button>')
+    const col_3_4 = $('<div class="col d-flex justify-content-center m-1">')
+    const erase_id = $('<button id="erase" class="fs-1 w-100 btn m-1 clickable" type="button"><i class="fa-solid fa-eraser"></i></button>')
+
+    const row_4 = $('<div class="row d-flex justify-content-center">')
+    const col_4_1 = $('<div class="col d-flex justify-content-center m-1">')
+    const seven_id = $('<button id="seven" class="fs-1 w-100 btn m-1 clickable" type="button"><span>7</span></button>')
+    const col_4_2 = $('<div class="col d-flex justify-content-center m-1">')
+    const eight_id = $('<button id="eight" class="fs-1 w-100 btn m-1 clickable" type="button"><span>8</span></button>')
+    const col_4_3 = $('<div class="col d-flex justify-content-center m-1">')
+    const nine_id = $('<button id="nine" class="fs-1 w-100 btn m-1 clickable" type="button"><span>9</span></button>')
+    const col_4_4 = $('<div class="col d-flex justify-content-center m-1">')
+    const btn_free = $('<button class="fs-1 w-100 btn m-1 clickable" type="button"></button>')
+
+    const row_5 = $('<div class="row d-flex justify-content-center">')
+    const col_5_1 = $('<div class="col d-flex justify-content-center m-1">')
+    const minus_id = $('<button id="minus" class="fs-1 w-100 btn m-1 clickable" type="button"><i class="fa-solid fa-plus-minus"></i></button>')
+    const col_5_2 = $('<div class="col d-flex justify-content-center m-1">')
+    const zero_id = $('<button id="zero" class="fs-1 w-100 btn m-1 clickable" type="button"><span>0</span></button>')
+    const col_5_3 = $('<div class="col d-flex justify-content-center m-1">')
+    const dot_id = $('<button id="dot" class="w-100 btn m-1 clickable" type="button"><span>.</span></button>')
+    const col_5_4 = $('<div class="col d-flex justify-content-center m-1">')
+    const enter_id = $('<button id="enter" class="fs-1 w-100 btn m-1 clickable" type="button"><i class="fa fa-solid fa-check"></i></button>')
+
+    modal.append(modal_dialog.append(modal_content.append(modal_body.append(container_lg))))
+    container_lg
+        .append(row_1
+            .append(col_1_1.append(keyboard_input_id)))
+        .append(row_2
+            .append(col_2_1.append(one_id))
+            .append(col_2_2.append(two_id))
+            .append(col_2_3.append(three_id))
+            .append(col_2_4.append(backspace_id)))
+        .append(row_3
+            .append(col_3_1.append(four_id))
+            .append(col_3_2.append(five_id))
+            .append(col_3_3.append(six_id))
+            .append(col_3_4.append(erase_id)))
+        .append(row_4
+            .append(col_4_1.append(seven_id))
+            .append(col_4_2.append(eight_id))
+            .append(col_4_3.append(nine_id))
+            .append(col_4_4.append(btn_free)))
+        .append(row_5
+            .append(col_5_1.append(minus_id))
+            .append(col_5_2.append(zero_id))
+            .append(col_5_3.append(dot_id))
+            .append(col_5_4.append(enter_id)))
+
+    body.append(modal)
+})
