f1dbca1bdd3164ea3375f27e7e73fd88239f12bb
Anatolii Kovrigin <44209048+AnatoliyGIT@users.noreply.github.com>
2022-11-21 09:00:17 +0200

    Update press.js

--- src/js/press.js
+++ src/js/press.js
@@ -17,6 +17,80 @@ $(function ($) {
     let additive_l = 0
     let additive_r = 0
 
+// Устанавливаем язык страницы в селекторе из адресной строки и наоборот
+    const lang = $("#lang-selector")
+    const link_back = $("#link-back")
+    const title_main = $("#title-main")
+
+    let set_select_by_value = function(select_id, option_val) {
+        document.getElementById(select_id).value = option_val
+    }
+
+    let getURLVarArr = function() {
+        var data = []
+        var query = String(document.location.href).split('?')
+        if (query[1]) {
+            var part = query[1].split('&')
+            for (i = 0; i < part.length; i++) {
+                var dat = part[i].split('=')
+                data[dat[0]] = dat[1]
+            }
+        }
+        return data
+    }
+
+    let get_value_of_change_select = function(select) {
+        let result
+        $.each(select, function (i) {
+            select.options[i].foo = function () {
+                result = this.value
+            }
+        })
+        select.options[select.selectedIndex].foo()
+        return result
+    }
+
+    let setLocation = function(curLoc){
+        try {
+            history.pushState(null, null, curLoc)
+            return
+        } catch(e) {}
+        location.hash = '#' + curLoc
+    }
+
+    let translate = function() {
+        if (lang_val === "en") {
+            title_main.html("Pressure")
+        }
+        if (lang_val === "he") {
+            title_main.html("לחץ")
+        }
+        if (lang_val === "ru") {
+            title_main.html("Давление")
+        }
+    }
+
+    let lang_val = getURLVarArr().lang
+
+    set_select_by_value("lang-selector", lang_val)
+
+    let language = get_value_of_change_select(lang[0])
+
+    lang.on("change", function() {
+        language = get_value_of_change_select(lang[0])
+        lang_val = getURLVarArr().lang
+        if (lang_val !== language) {
+            setLocation("?lang=" + language)
+            lang_val = getURLVarArr().lang
+            link_back.attr("href", "index.html?lang=" + lang_val)
+        }
+        translate()
+    })
+    translate()
+
+    link_back.attr("href", "index.html?lang=" + lang_val)
+// End
+
 // Обходим левый селектор в input записываем введенное значение, в unit_l записываем value текущего селектора в Мегапаскалях
     $.each(select_l, function (i) {
         select_l.options[i].foo = function () {
